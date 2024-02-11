b47b1efff354fda7afecf90cce73d0ce4d8a47f0
faris <ubaidillah.faris99@gmail.com>
2024-02-11 11:53:09 +0700

    hotfix: config invoice generator

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use LaravelDaily\Invoices\Invoice;
 use LaravelDaily\Invoices\Classes\Buyer;
+use LaravelDaily\Invoices\Classes\Party;
 use LaravelDaily\Invoices\Classes\InvoiceItem;
 
-use App\Models\Invoices;
+use App\Models\Invoices as InvoiceModel;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class InvoiceController extends Controller
 {
@@ -68,7 +70,7 @@ class InvoiceController extends Controller
 
             $invoiceData = array_filter($invoiceData);
 
-            $invoice = Invoice::create($invoiceData);
+            $invoice = InvoiceModel::create($invoiceData);
 
 
             DB::commit();
@@ -85,21 +87,79 @@ class InvoiceController extends Controller
     }
 
     public function stream() {
-        $customer = new Buyer([
-            'name'          => 'John Doe',
+        $dari = new Party([
+            'name'          => 'Roosevelt Lloyd',
+            'phone'         => '(520) 318-9486',
             'custom_fields' => [
-                'email' => 'test@example.com',
+                'note'        => 'IDDQD',
+                'business id' => '365#GG',
             ],
         ]);
         
-        $item = InvoiceItem::make('Service 1')->pricePerUnit(2);
+        $kepada = new Party([
+            'name'          => 'Ashley Medina',
+            'address'       => 'The Green Street 12',
+            'code'          => '#22663214',
+            'custom_fields' => [
+                'order number' => '> 654321 <',
+            ],
+        ]);
+        $items = [
+            InvoiceItem::make('Service 1')
+                ->description('Your product or service description')
+                ->pricePerUnit(47.79)
+                ->quantity(2)
+                ->discount(10),
+            InvoiceItem::make('Service 2')->pricePerUnit(71.96)->quantity(2)->tax(8),
+            InvoiceItem::make('Service 3')->pricePerUnit(4.56),
+            InvoiceItem::make('Service 4')->pricePerUnit(87.51)->quantity(7)->discount(4)->units('kg'),
+            InvoiceItem::make('Service 5')->pricePerUnit(71.09)->quantity(7)->discountByPercent(9),
+            InvoiceItem::make('Service 6')->pricePerUnit(76.32)->quantity(9),
+            InvoiceItem::make('Service 7')->pricePerUnit(58.18)->quantity(3)->discount(3),
+            InvoiceItem::make('Service 8')->pricePerUnit(42.99)->quantity(4)->discountByPercent(3),
+            InvoiceItem::make('Service 9')->pricePerUnit(33.24)->quantity(6)->units('m2'),
+            InvoiceItem::make('Service 11')->pricePerUnit(97.45)->quantity(2),
+            InvoiceItem::make('Service 12')->pricePerUnit(92.82),
+            InvoiceItem::make('Service 13')->pricePerUnit(12.98),
+            InvoiceItem::make('Service 14')->pricePerUnit(160)->units('hours'),
+            InvoiceItem::make('Service 15')->pricePerUnit(62.21)->discountByPercent(5),
+            InvoiceItem::make('Service 16')->pricePerUnit(2.80),
+            InvoiceItem::make('Service 17')->pricePerUnit(56.21),
+            InvoiceItem::make('Service 18')->pricePerUnit(66.81)->discountByPercent(8),
+            InvoiceItem::make('Service 19')->pricePerUnit(76.37),
+            InvoiceItem::make('Service 20')->pricePerUnit(55.80),
+        ];
         
-        $invoice = Invoice::make()
-            ->buyer($customer)
-            ->discountByPercent(10)
-            ->taxRate(15)
-            ->shipping(1.99)
-            ->addItem($item);
-        return $invoice->toHtml();
+        $notes = [
+            'your multiline',
+            'additional notes',
+            'in regards of delivery or something else',
+        ];
+        $notes = implode("<br>", $notes);
+        
+        $invoice = Invoice::make('receipt')
+            ->series('BIG')
+            ->status(__('BELUM BAYAR'))
+            ->sequence(667)
+            ->serialNumberFormat('{SEQUENCE}/{SERIES}')
+            ->seller($dari)
+            ->buyer($kepada)
+            ->date(now()->subWeeks(3))
+            ->dateFormat('m/d/Y')
+            ->payUntilDays(14)
+            ->currencySymbol('Rp.')
+            ->currencyCode('IDR')
+            ->currencyFormat('{SYMBOL}{VALUE}')
+            ->currencyThousandsSeparator('.')
+            ->currencyDecimalPoint(',')
+            ->filename($dari->name . ' ' . $kepada->name)
+            ->addItems($items)
+            ->notes($notes)
+            ->logo(public_path('vendor/invoices/sample-logo.png'))
+            ->save('public');
+        
+            $invoice->save('public');
+            $link = Storage::url($dari->name . ' ' . $kepada->name).'.pdf';
+            return $link;
     }
 }
